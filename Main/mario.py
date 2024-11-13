35a3e33434baa5f80f038789d8e6d09e7ec43db6
Hamzzis99 <hamzzi99@naver.com>
2024-11-13 23:21:37 +0900

    속력설정
    아직 성공못함.

--- Main/mario.py
+++ Main/mario.py
@@ -3,6 +3,29 @@ from pico2d import load_image
 from state_machine import StateMachine, right_down, left_down, right_up, left_up, start_event
 
 
+# Boy Run Speed
+#PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm #기본 단위 변환
+#RUN_SPEED_KMPH = 20.0  # Km / Hour
+#RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+#RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+#RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# 설정된 값
+x_coords = [290, 304, 321]
+y_coord = 342
+width, height = 16, 16
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
+# Boy Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Mario:
     def __init__(self):
         self.x, self.y = 400, 90
